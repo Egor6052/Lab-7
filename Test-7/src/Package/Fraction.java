7f1398240f62403cc47b399010b62fe2188cc1f8
Egor6052 <101550518+Egor6052@users.noreply.github.com>
2022-05-16 11:22:27 +0300

    Lab-7
    Lab-7

---
+++ Test-7/src/Package/Fraction.java
@@ -0,0 +1,86 @@
+package Package;
+
+public class Fraction {
+    static double a1;
+    static double a2;
+    static double a3;
+    static double b1;
+    static double b2;
+    static double b3;
+
+    public Fraction(double a1, double a2, double a3, double b1, double b2, double b3) {
+        Fraction.a1 = a1;
+        Fraction.a2 = a2;
+        Fraction.a3 = a3;
+        Fraction.b1 = b1;
+        Fraction.b2 = b2;
+        Fraction.b3 = b3;
+    }
+
+    public static double setA1(double a1) {
+        return Fraction.a1 = a1;
+    }
+
+    public static double setA2(double a2) {
+       return Fraction.a2 = a2;
+    }
+
+    public static double setA3(double a3) {
+        return Fraction.a3 = a3;
+    }
+
+    public static double setB1(double b1) {
+        return Fraction.b1 = b1;
+    }
+
+    public static double setB2(double b2) {
+        return Fraction.b2 = b2;
+    }
+
+    public static double setB3(double b3) {
+        return Fraction.b3 = b3;
+    }
+
+    public static double getA1() {
+        return a1;
+    }
+
+    public static double getA2() {
+        return a2;
+    }
+
+    public static double getA3() {
+        return a3;
+    }
+
+    public static double getB1() {
+        return b1;
+    }
+
+    public static double getB2() {
+        return b2;
+    }
+
+    public static double getB3() {
+        return b3;
+    }
+
+    //-------------------------------------------
+
+    public static double R() {
+        return Math.ceil((a1 / b1 + a2 / b2) * (a1 / b1 - a3 / b3));
+    }
+
+    public static double R1() {
+        return Math.ceil(1 / (R()));
+    }
+
+    @Override
+    public String toString() {
+        return "R = " + Correct.formatDouble(R()) + "\n" +
+                "1/R = " + Correct.formatDouble(R1()) + "\n\n\n";
+    }
+
+
+}
+
---
+++ Test-7/src/Package/Main.java
@@ -0,0 +1,31 @@
+package Package;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void Task7() {
+        System.out.println("Семенов Егор АТ-212, Вариант №7");
+
+//        Fraction.a1 = Math.ceil(1 + Math.random() * 10);
+//        Fraction.a2 = Math.ceil(1 + Math.random() * 10);
+//        Fraction.a3 = Math.ceil(1 + Math.random() * 10);
+//        Fraction.b1 = Math.ceil(1 + Math.random() * 10);
+//        Fraction.b3 = Math.ceil(1 + Math.random() * 10);
+//        Fraction.b1 = Math.ceil(1 + Math.random() * 10);
+
+
+        Fraction.setA1(Math.ceil(1 + Math.random() * 10));
+        Fraction.setA2(Math.ceil(1 + Math.random() * 10));
+        Fraction.setA3(Math.ceil(1 + Math.random() * 10));
+        Fraction.setB1(Math.ceil(1 + Math.random() * 10));
+        Fraction.setB2(Math.ceil(1 + Math.random() * 10));
+        Fraction.setB3(Math.ceil(1 + Math.random() * 10));
+
+
+        System.out.println(Fraction.R());
+        System.out.println(Fraction.R1());
+    }
+    public static void main(String[] args) {
+        Task7();
+    }
+}
