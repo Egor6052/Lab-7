458b0f508641ce952b27f81e17b45566c04bb5c6
Egor6052 <101550518+Egor6052@users.noreply.github.com>
2022-05-27 21:53:06 +0300

    Update Fraction.java

--- Test-7/src/Package/Fraction.java
+++ Test-7/src/Package/Fraction.java
@@ -1,78 +1,47 @@
 package Package;
 
 public class Fraction {
-    static double a1;
-    static double a2;
-    static double a3;
-    static double b1;
-    static double b2;
-    static double b3;
+    static double x1;
+    static double x2;
+    static double x3;
 
-    public Fraction(double a1, double a2, double a3, double b1, double b2, double b3) {
-        Fraction.a1 = a1;
-        Fraction.a2 = a2;
-        Fraction.a3 = a3;
-        Fraction.b1 = b1;
-        Fraction.b2 = b2;
-        Fraction.b3 = b3;
+    public Fraction(final double x1, final double x2, final double x3) {
+        this.setX1(x1);
+        this.setX2(x2);
+        this.setX3(x3);
     }
 
-    public static double setA1(double a1) {
-        return Fraction.a1 = a1;
+    public void setX1(double x1) {
+        Fraction.x1 = x1;
     }
 
-    public static double setA2(double a2) {
-        return Fraction.a2 = a2;
+    public void setX2(double x2) {
+        Fraction.x2 = x2;
     }
 
-    public static double setA3(double a3) {
-        return Fraction.a3 = a3;
+    public void setX3(double x3) {
+        Fraction.x3 = x3;
     }
 
-    public static double setB1(double b1) {
-        return Fraction.b1 = b1;
+    public static double getX1() {
+        return x1;
     }
 
-    public static double setB2(double b2) {
-        return Fraction.b2 = b2;
+    public double getX2() {
+        return x2;
     }
 
-    public static double setB3(double b3) {
-        return Fraction.b3 = b3;
+    public double getX3() {
+        return x3;
     }
-
-    public static double getA1() {
-        return a1;
-    }
-
-    public static double getA2() {
-        return a2;
-    }
-
-    public static double getA3() {
-        return a3;
-    }
-
-    public static double getB1() {
-        return b1;
-    }
-
-    public static double getB2() {
-        return b2;
-    }
-
-    public static double getB3() {
-        return b3;
-    }
-
     //-------------------------------------------
 
-    public static double R() {
-        return (a1 / b1 + a2 / b2) * (a1 / b1 - a3 / b3);
+    public static double R(Fraction PointA, Fraction PointB) {
+        return (PointA.getX1() / PointB.getX1() + PointA.getX2() / PointB.getX2()) * (PointA.getX1() / PointB.getX1() - PointA.getX3() / PointB.getX3());
     }
 
-    public static double R1() {
-        return 1 / (a1 / b1 + a2 / b2) * (a1 / b1 - a3 / b3);
+    public static double R1(Fraction PointA, Fraction PointB) {
+        return 1 / (PointA.getX1() / PointB.getX1() + PointA.getX2() / PointB.getX2()) * (PointA.getX1() / PointB.getX1() - PointA.getX3() / PointB.getX3());
     }
 
     @Override
