31afd303d799fe17c120beaa67ecc4fc41945e73
Egor6052 <101550518+Egor6052@users.noreply.github.com>
2022-05-27 21:53:43 +0300

    Update Main.java

--- Test-7/src/Package/Main.java
+++ Test-7/src/Package/Main.java
@@ -4,9 +4,10 @@ public class Main {
     public static void Task7() {
         System.out.println("Семенов Егор АТ-212, Вариант №7");
 
-        Fraction Frac = new Fraction(Math.random() * 10, Math.random() * 10, Math.random() * 10, Math.random() * 10, Math.random() * 10, Math.random() * 10);
-        System.out.println(Frac);
-
+        Fraction PointA = new Fraction(Math.random() * 10, Math.random() * 10, Math.random() * 10);
+        Fraction PointB = new Fraction(Math.random() * 10, Math.random() * 10, Math.random() * 10);
+        System.out.println(PointA);
+        System.out.println(PointB);
     }
 
     public static void main(String[] args) {
