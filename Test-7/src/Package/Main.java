3a816b06660b3d6d29159a7a1f251ec970c3596e
Egor6052 <101550518+Egor6052@users.noreply.github.com>
2022-05-27 19:39:22 +0300

    Update Main.java

--- Test-7/src/Package/Main.java
+++ Test-7/src/Package/Main.java
@@ -1,23 +1,14 @@
 package Package;
 
-import java.util.Scanner;
-
 public class Main {
     public static void Task7() {
         System.out.println("Семенов Егор АТ-212, Вариант №7");
 
+        Fraction Frac = new Fraction(Math.random() * 10, Math.random() * 10, Math.random() * 10, Math.random() * 10, Math.random() * 10, Math.random() * 10);
+        System.out.println(Frac);
 
-        Fraction.setA1(Math.ceil(1 + Math.random() * 10));
-        Fraction.setA2(Math.ceil(1 + Math.random() * 10));
-        Fraction.setA3(Math.ceil(1 + Math.random() * 10));
-        Fraction.setB1(Math.ceil(1 + Math.random() * 10));
-        Fraction.setB2(Math.ceil(1 + Math.random() * 10));
-        Fraction.setB3(Math.ceil(1 + Math.random() * 10));
-
-
-        System.out.println(Fraction.R());
-        System.out.println(Fraction.R1());
     }
+
     public static void main(String[] args) {
         Task7();
     }
